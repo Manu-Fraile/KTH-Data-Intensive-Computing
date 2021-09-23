b534e4bba54602a2d44cf1a5f866907cc3b7e406
Riccardo Romagnoli <riccardo.romagnoli.1998@gmail.com>
2021-09-23 17:54:36 +0200

    First starting point

--- lab1/src/main/scala/main/Main.scala
+++ lab1/src/main/scala/main/Main.scala
@@ -3,6 +3,7 @@ package main
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
 
 object Main extends App {
   println("Hello, World!")
@@ -22,5 +23,12 @@ object Main extends App {
   val sparkConf = new SparkConf()
   // val sc = new SparkContext(sparkConf)
   val sc = spark.sparkContext
-  val pagecounts = sc.textFile("../../../../../pagecounts-20160101-000000_parsed.out")
+  val pagecounts = sc.textFile("pagecounts-20160101-000000_parsed.out")
+
+  case class Log(project_code:String, page_title:String, page_hits:Int, page_size:Long)
+
+  val data_log = pagecounts.map(row => row.split(' ')).map(field => Log(field(0), field(1), field(2).toInt, field(3).toLong))  
+  
+  // Print the RDD for debugging (this works, shows 2 dogs) 
+  data_log.collect().foreach(println)
 }

162c857554a91ba86f6a4e20d646b94615758ca5
Riccardo Romagnoli <riccardo.romagnoli.1998@gmail.com>
2021-09-23 13:54:14 +0200

    See if this spark init works on your side

--- lab1/src/main/scala/main/Main.scala
+++ lab1/src/main/scala/main/Main.scala
@@ -7,9 +7,20 @@ import org.apache.spark.SparkContext
 object Main extends App {
   println("Hello, World!")
 
-  val spark = SparkSession.builder().getOrCreate()
+//  val spark = SparkSession.builder().getOrCreate()
+//
+//  val sparkConf = new SparkConf()
+//  val sc = new SparkContext(sparkConf)
+//  val pagecounts = sc.textFile("../../../../../pagecounts-20160101-000000_parsed.out")
 
+  val spark = SparkSession.builder()
+                .appName("Spark-Demo")//assign a name to the spark application
+                .master("local[*]") //utilize all the available cores on local
+                .getOrCreate()
+
+  println("Hello, World 2!")
   val sparkConf = new SparkConf()
-  val sc = new SparkContext(sparkConf)
+  // val sc = new SparkContext(sparkConf)
+  val sc = spark.sparkContext
   val pagecounts = sc.textFile("../../../../../pagecounts-20160101-000000_parsed.out")
 }

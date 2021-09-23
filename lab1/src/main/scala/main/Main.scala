353483a32e2f2e7bc913357895650c7b674763c2
Riccardo Romagnoli <riccardo.romagnoli.1998@gmail.com>
2021-09-23 19:41:18 +0200

    till q7, even

--- lab1/src/main/scala/main/Main.scala
+++ lab1/src/main/scala/main/Main.scala
@@ -27,8 +27,64 @@ object Main extends App {
 
   case class Log(project_code:String, page_title:String, page_hits:Int, page_size:Long)
 
-  val data_log = pagecounts.map(row => row.split(' ')).map(field => Log(field(0), field(1), field(2).toInt, field(3).toLong))  
-  
-  // Print the RDD for debugging (this works, shows 2 dogs) 
-  data_log.collect().foreach(println)
+  val data_log = pagecounts.map(row => row.split(' ')).map(field => Log(field(0), field(1), field(2).toInt, field(3).toLong))
+
+  def q1(): Unit ={
+    data_log.take(15).foreach(println)
+  }
+  q1()
+
+  def q2(): Unit ={
+    println(data_log.collect().size)
+  }
+  q2()
+
+  //3. Compute the min, max, and average page size.
+  def q3(): Unit ={
+    var page_sizes = data_log.map(_.page_size)
+    var min = page_sizes.min()
+    var max = page_sizes.max()
+    var avg = page_sizes.sum() / page_sizes.collect().size
+    println(s"$min, $max, $avg")
+  }
+  q3()
+
+  //5. Determine the record with the largest page size again. But now, pick the most popular.
+  def q5(): Unit ={
+    var page_sizes = data_log.map(_.page_size)
+    var page_hits = data_log.map(_.page_hits)
+    var max_size = page_sizes.max()
+    var max_hits = page_hits.max()
+    data_log.filter(item => (item.page_size.equals(max_size) && item.page_hits.equals(max_hits))).collect().take(1).foreach(println)
+  }
+  q5()
+
+  //7. Use the results of Question 3, and create a new RDD with the records that have greater page size than
+  //the average.
+  def q7(): Unit ={
+    var page_sizes = data_log.map(_.page_size)
+    var avg = page_sizes.sum() / page_sizes.collect().size
+    var greater_avg = data_log.filter(item => item.page_size > avg).collect()
+    //greater_avg.foreach(println)
+  }
+  q7()
+
+  //9. Report the 10 most popular pageviews of all projects, sorted by the total number of hits.
+  def q9(): Unit ={
+
+  }
+  q9()
+
+  //11. Determine the percentage of pages that have only received a single page view in this one hour of log
+  //data.
+  def q11(): Unit ={
+
+  }
+  q11()
+
+  //13. Determine the most frequently occurring page title term in this dataset.
+  def q13(): Unit ={
+
+  }
+  q13()
 }

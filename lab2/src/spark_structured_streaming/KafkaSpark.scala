88152bfa39c149dc0d4324399173524cdba68239
Riccardo Romagnoli <riccardo.romagnoli.1998@gmail.com>
2021-10-06 16:26:36 +0200

    Task 2 Done

--- lab2/src/spark_structured_streaming/KafkaSpark.scala
+++ lab2/src/spark_structured_streaming/KafkaSpark.scala
@@ -10,9 +10,14 @@ import org.apache.spark.SparkConf
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka._
 import org.apache.spark.storage.StorageLevel
+import org.apache.spark.sql.functions._
 import java.util.{Date, Properties}
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord, ProducerConfig}
 import scala.util.Random
+import org.apache.spark.sql.streaming._
+
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.SparkSession
 
 object KafkaSpark {
   def main(args: Array[String]) {
@@ -24,50 +29,40 @@ object KafkaSpark {
     // make a connection to Kafka and read (key, value) pairs from it
     val conf = new SparkConf().setMaster("local[2]").setAppName("AVG")
     val ssc = new StreamingContext(conf, Seconds(1))
-
     ssc.checkpoint("checkpoint")
+
+    val spark = SparkSession.builder().getOrCreate()
+    import spark.implicits._
+
+    val schema = StructType(Array(StructField("key", StringType), StructField("value", DoubleType)))
+    case class Pair(key:String, value:Double)
+
+    val df = spark
+      .readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092, localhost:2181")
+      .option("subscribe", "avg")
+      .load()
+    val df_parsed = df.selectExpr("CAST(value AS STRING)").as[(String)]
+
+    //create new columns, parse out the orig message and fill column with the values
+    val df_schema = df_parsed.selectExpr("value",
+                                "split(value,',')[0] as key",
+                                "split(value,',')[1] as value1")
+                                      .drop("value");
+    //val df_schema = df_parsed.map(row => row.split(',')).map(field => Pair(field(0), field(1).toDouble))
+
     
-    val kafkaConf = Map(
-      "metadata.broker.list" -> "localhost:9092",
-      "zookeeper.connect" -> "localhost:2181",
-      "group.id" -> "kafka-spark-streaming",
-      "zookeeper.connection.timeout.ms" -> "1000"
-    )
-    val topics = Set("avg")
-    val messages = KafkaUtils.createDirectStream[String,String,StringDecoder,StringDecoder](ssc, kafkaConf, topics)
-
-    val pairs = messages.map(x => {
-      val s = x._2.split(",")
-      (s(0), s(1).toDouble)
-    })
-
-
-    // measure the average value for each key in a stateful manner
-    def mappingFunc(key: String, value: Option[Double], state: State[(Int, Double)]): (String, Double) = {
-      if (state.exists()) {
-
-        val (count, mean) = state.get()
-        val newCount = count + 1
-        val differential = (value.get - mean) / newCount
-        val newMean = mean + differential
-        val newState = (newCount, newMean)
-        state.update(newState)
-
-        (key, newMean)
-
-      } else {
-        state.update(1, value.get)
-        (key, value.get)
-      }
-    }
-
-    // Attach the mapping function
-    val stateDstream = pairs.mapWithState(StateSpec.function(mappingFunc _)).map{case (key, mean) => {
-      (key, mean)
-    }}
-    stateDstream.print()
-
-    ssc.start()
-    ssc.awaitTermination()
+    df_schema.createOrReplaceTempView("dfView")
+    val res = spark.sql("SELECT key, AVG(value1) AS avg FROM dfView GROUP BY key") // returns another streaming DF
+    
+    res.writeStream
+    .trigger(Trigger.ProcessingTime("1 second"))
+    .option("checkpointLocation", "checkpoint")
+    .outputMode("complete")
+    .format("console")
+    .option("truncate","false")
+    .start()
+    .awaitTermination()
   }
 }
--- lab2/src/spark_structured_streaming/build.sbt
+++ lab2/src/spark_structured_streaming/build.sbt
@@ -5,6 +5,7 @@ version := "1.0"
 scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
+  "org.apache.spark" % "spark-sql-kafka-0-10_2.11" % "2.4.3",
   "org.apache.spark" %% "spark-core" % "2.4.3",
   "org.apache.spark" %% "spark-sql" % "2.4.3",
   "org.apache.spark" % "spark-streaming_2.11" % "2.4.3",
